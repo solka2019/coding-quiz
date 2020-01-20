b172974af0315fc018cd4ec94306f8291bdfd913
Marissol Karczeski <marfkar@gmail.com>
2020-01-19 16:50:52 -0800

    start code

---
+++ questions.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choiceA: "strings",
+      choiceB: "booleans",
+      choiceC: "alerts",
+      choiceD: "numbers",
+      correct: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choiceA: "quotes",
+        choiceB: "curly brackets",
+        choiceC: "parentheses",
+        choiceD: "square brackets",
+        correct: "parentheses"
+    },
+    {
+        question: "A letter, $ or_",
+        choiceA: "What is a variable?",
+        choiceB: "What kind of value does Boolean take?",
+        choiceC: "What will an undeclared variable return?",
+        choiceD: "What must a variable begin with?",
+        correct: "What must a variable begin with?"
+
+    },
+    {
+        question: "Which of the following is the correct syntax to display “Hello” in an alert box using JavaScript?",
+        choiceA: "alertbox(“Hello”)",
+        choiceB: "msg(“Hello”)",
+        choiceC: "msgbox(“Hello”)",
+        choiceD: "alert(“Hello”)",
+        correct: "alert(“Hello”)"
+    },
+    {
+        question: "Internally using html <script></script> tag. Best practice to place it before closing body tag .",
+      choiceA: "How do you add JS To your site internally?",
+      choiceB: "How do you add JS to your site externally?",
+      choiceC: "How do you declare variables?",
+      choiceD: "What are the two ways to add JS to your site?",
+      correct: "How do you add JS To your site internally?"
+    {
+  ];
---
+++ script.js
@@ -0,0 +1,103 @@
+let start = document.getElementById("start");
+let quiz = document.getElementById("quiz");
+let question = document.getElementById("question");
+let counter = document.getElementById("counter");
+
+let choiceA = document.getElementById("A");
+let choiceB = document.getElementById("B");
+let choiceC = document.getElementById("C");
+let choiceD = document.getElementById("D");
+
+let questionTime = 15;
+let count = 0;
+
+let TIMER =
+    setInterval(counterRender, 1000);
+// run the counterRender every 1sec
+// stop running : setInterval()
+clearInterval(TIMER);
+
+
+
+questions[0].question
+questions[0].choiceA
+questions[0].choiceB
+questions[0].choiceC
+questions[0].choiceD
+questions[0].correct
+
+let lastQuestionIndex = questions.length - 1;
+// array.length -> 4 
+// array = [a,b,c,d]
+let runningQuestionIndex = 0;
+// above is the question the user is on
+// the next question will be the current, increment by 1 ++
+// runningQuestionIndex++
+// renderQuestion()
+
+
+function renderQuestion() {
+    let q = questions[runningQuestionIndex];
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+}
+// runningQuestionIndex = 0;
+// renderQuestion()
+// runningQuestionIndex++
+// renderQuestion()
+function counterRender() {
+    if (count <= questionTime) {
+        counter.innerHTML = count;
+        count++;
+    } else {
+        // if the user exceed the question time, we need to set the counter to 0
+        // if the user doesn't answer the question because of time, the answer is wrong
+        count = 0;
+        answerIsWrong();
+
+        if (runningQuestionIndex < lastQuestionIndex) {
+            // if there are still questions, it is true, then do the next step
+            runningQuestionIndex++;
+            questionRender();
+        } else {
+            clearInterval(TIMER);
+            scoreRender();
+            // show the score to the user - scoreRender
+        }
+    }
+}
+
+function checkAnswer(answer) {
+    if (questions[runningQuestionIndex].correct == answer) {
+        score++;
+        answerIsCorrect();
+
+
+    } else {
+        answerIsWrong();
+    }
+    if (runningQuestionIndex < lastQuestionIndex) {
+        count = 0;
+        runningQuestionIndex++;
+        questionRender();
+
+
+    } else {
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+start.addEventListener("click", startQuiz);
+let TIMER;
+// it is outside the function, so any other function can access the timer
+function startQuiz(){
+    start.style.display = "none";
+    counterRender();
+    TIMER = setInterval(counterRender, 1000);
+    questionRender();
+    quiz.style.display = "block";
+    // in my html, I set the display style to "none"
+
